48285b30e800b0e59ce90dd3ee244332b6cb2db4
max <cmendoza2002@gmail.com>
2019-09-23 10:41:06 -0500

    Primera Subida

---
+++ evaluador/src/main/java/com/maint/calc/evaluador/EvaluadorApplication.java
@@ -0,0 +1,13 @@
+package com.maint.calc.evaluador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluadorApplication.class, args);
+	}
+
+}
