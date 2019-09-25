9d87602955b86d54207b1ca16e2e6db7ddab9cca
carlos <cclozano@gmail.com>
2019-09-25 16:33:16 -0500

    Metodo evaluar expresion

--- evaluador/src/main/java/com/maint/calc/evaluador/EvaluadorController.java
+++ evaluador/src/main/java/com/maint/calc/evaluador/EvaluadorController.java
@@ -1,5 +1,7 @@
 package com.maint.calc.evaluador;
 
+import com.maint.calc.evaluador.rest.EvaluadorRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,12 @@ import java.math.BigDecimal;
 @RestController
 public class EvaluadorController {
 
-    @RequestMapping(value = "/evaluar", method = RequestMethod.GET)
-    public BigDecimal evaluarExprecion(String expresion)
+    @RequestMapping(value = "/evaluar", method = RequestMethod.POST)
+    public boolean evaluarExprecion(@RequestBody EvaluadorRequest request)
     {
-        return BigDecimal.ZERO;
+        java.util.regex.Pattern pattern =
+                java.util.regex.Pattern.compile("(\\(*[0-9]+[+\\-*][0-9]\\)*)+([+\\-*][0-9])*");
+        java.util.regex.Matcher matcher = pattern.matcher(request.getExpresion());
+        return matcher.matches();
     }
 }
---
+++ evaluador/src/main/java/com/maint/calc/evaluador/rest/EvaluadorRequest.java
@@ -0,0 +1,9 @@
+package com.maint.calc.evaluador.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class EvaluadorRequest {
+    private String expresion;
+}
