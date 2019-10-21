4525ce1b42adbf1f5f54d943a04ecab7c145ad34
Carlos Mendoza <cmendoza2002@gmail.com>
2019-10-21 13:27:44 -0500

    Soporte para ansible agregado

--- ui/src/main/java/com/maint/calc/ui/servicios/EvaluadorServiceImp.java
+++ ui/src/main/java/com/maint/calc/ui/servicios/EvaluadorServiceImp.java
@@ -8,12 +8,13 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class EvaluadorServiceImp implements EvaluadorService{
 
+    private static final String API_SERVER_IP = "192.168.7.130:9090";
 
 
     @Override
     public double procesar(String expresion) {
         RestTemplate restTemplate = new RestTemplate();
-        OperacionResponse response = restTemplate.postForObject("http://localhost:9090/calculadora/v1/evaluador/procesar",new EvaluadorRequest(expresion), OperacionResponse.class);
+        OperacionResponse response = restTemplate.postForObject("http://"+API_SERVER_IP+"/calculadora/v1/evaluador/procesar",new EvaluadorRequest(expresion), OperacionResponse.class);
         return response.getResultado();
     }
 }
