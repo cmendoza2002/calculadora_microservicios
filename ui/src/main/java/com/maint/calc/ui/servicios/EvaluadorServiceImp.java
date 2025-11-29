da1bfc6fa46b41a6d96592461d84689fda28566f
Carlos R. Lozano <clozano@technisys.com>
2025-11-29 08:43:37 -0500

    feat: Introduce Dockerfiles and docker-compose for microservice containerization and orchestration, and externalize UI gateway URL.

--- ui/src/main/java/com/maint/calc/ui/servicios/EvaluadorServiceImp.java
+++ ui/src/main/java/com/maint/calc/ui/servicios/EvaluadorServiceImp.java
@@ -2,19 +2,22 @@ package com.maint.calc.ui.servicios;
 
 import com.maint.calc.ui.infraestructura.EvaluadorRequest;
 import com.maint.calc.ui.infraestructura.OperacionResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class EvaluadorServiceImp implements EvaluadorService{
+public class EvaluadorServiceImp implements EvaluadorService {
 
-    private static final String API_SERVER_IP = "192.168.7.130:9090";
-
+    @Value("${gateway.url:192.168.7.130:9090}")
+    private String apiServerIp;
 
     @Override
     public double procesar(String expresion) {
         RestTemplate restTemplate = new RestTemplate();
-        OperacionResponse response = restTemplate.postForObject("http://"+API_SERVER_IP+"/calculadora/v1/evaluador/procesar",new EvaluadorRequest(expresion), OperacionResponse.class);
+        OperacionResponse response = restTemplate.postForObject(
+                "http://" + apiServerIp + "/calculadora/v1/evaluador/procesar", new EvaluadorRequest(expresion),
+                OperacionResponse.class);
         return response.getResultado();
     }
 }
